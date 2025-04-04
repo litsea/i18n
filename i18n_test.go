bbb37110cdad2ed2004bb3e946ac69816903b50c
larryannon <143927678+larryannon@users.noreply.github.com>
2025-04-04 23:51:20 +0800

    improve load msg error log (#2)

--- i18n_test.go
+++ i18n_test.go
@@ -1,11 +1,14 @@
 package i18n
 
 import (
+	"log/slog"
+	"strings"
 	"testing"
 
-	"github.com/litsea/i18n/testdata"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/text/language"
+
+	"github.com/litsea/i18n/testdata"
 )
 
 type args struct {
@@ -234,16 +237,20 @@ func TestTranslate(t *testing.T) {
 
 		name = "FileLoader-" + tt.args.lng.String() + "-" + tt.args.msgID + "-" + tt.name
 		runTranslationTest(t, tt, name,
-			FileLoader(defaultLocalizeFileDir),
+			WithLoaders(FileLoader(defaultLocalizeFileDir)),
 		)
 
 		name = "EmbedLoader-" + tt.args.lng.String() + "-" + tt.args.msgID + "-" + tt.name
 		runTranslationTest(t, tt, name,
-			EmbedLoader(testdata.Localize, "./localize/"),
+			WithLoaders(EmbedLoader(testdata.Localize, "./localize/")),
 		)
 	}
+}
 
-	overrideTests := []testTranslation{
+func TestTranslateOverride(t *testing.T) {
+	t.Parallel()
+
+	tests := []testTranslation{
 		{
 			name: "hello",
 			args: args{
@@ -251,6 +258,17 @@ func TestTranslate(t *testing.T) {
 				msgID: "welcome",
 			},
 			want: "hello2",
+		},
+		{
+			name: "origin hello alex",
+			args: args{
+				lng:   language.English,
+				msgID: "welcomeWithName",
+				tplData: map[any]any{
+					"name": "alex",
+				},
+			},
+			want: "hello alex",
 		},
 		{
 			name: "hello",
@@ -261,6 +279,17 @@ func TestTranslate(t *testing.T) {
 			want: "hallo2",
 		},
 		{
+			name: "origin hello alex",
+			args: args{
+				lng:   language.German,
+				msgID: "welcomeWithName",
+				tplData: map[any]any{
+					"name": "alex",
+				},
+			},
+			want: "hallo alex",
+		},
+		{
 			name: "hello",
 			args: args{
 				lng:   language.French,
@@ -269,44 +298,69 @@ func TestTranslate(t *testing.T) {
 			want:    "hello2",
 			errFunc: IsMessageFallbackToDefaultErr,
 		},
+		{
+			name: "origin hello alex",
+			args: args{
+				lng:   language.French,
+				msgID: "welcomeWithName",
+				tplData: map[any]any{
+					"name": "alex",
+				},
+			},
+			want:    "hello alex",
+			errFunc: IsMessageFallbackToDefaultErr,
+		},
 	}
 
-	for _, tt := range overrideTests {
+	for _, tt := range tests {
 		name := "FileLoader-Override-" + tt.args.lng.String() + "-" + tt.args.msgID + "-" + tt.name
 		runTranslationTest(t, tt, name,
-			FileLoader(defaultLocalizeFileDir),
-			FileLoader("./testdata/localize-override/"),
+			WithLoaders(
+				FileLoader(defaultLocalizeFileDir),
+				FileLoader("./testdata/localize-override/"),
+			),
 		)
 
 		name = "EmbedLoader-Override-" + tt.args.lng.String() + "-" + tt.args.msgID + "-" + tt.name
 		runTranslationTest(t, tt, name,
-			EmbedLoader(testdata.Localize, "./localize/"),
-			EmbedLoader(testdata.LocalizeOverride, "./localize-override/"),
+			WithLoaders(
+				EmbedLoader(testdata.Localize, "./localize/"),
+				EmbedLoader(testdata.LocalizeOverride, "./localize-override/"),
+			),
 		)
 
 		name = "File-Embed-Override-" + tt.args.lng.String() + "-" + tt.args.msgID + "-" + tt.name
 		runTranslationTest(t, tt, name,
-			FileLoader(defaultLocalizeFileDir),
-			EmbedLoader(testdata.LocalizeOverride, "./localize-override/"),
+			WithLoaders(
+				FileLoader(defaultLocalizeFileDir),
+				EmbedLoader(testdata.LocalizeOverride, "./localize-override/"),
+			),
 		)
 
 		name = "Embed-File-Override-" + tt.args.lng.String() + "-" + tt.args.msgID + "-" + tt.name
 		runTranslationTest(t, tt, name,
-			EmbedLoader(testdata.Localize, "./localize/"),
-			FileLoader("./testdata/localize-override/"),
+			WithLoaders(
+				EmbedLoader(testdata.Localize, "./localize/"),
+				FileLoader("./testdata/localize-override/"),
+			),
 		)
 	}
 }
 
-func runTranslationTest(t *testing.T, tt testTranslation, name string, ls ...Loader) {
+func runTranslationTest(t *testing.T, tt testTranslation, name string, opts ...Option) {
 	t.Run(name, func(t *testing.T) {
 		t.Parallel()
 
-		i := New(
+		ops := []Option{
 			WithLanguages(tt.args.lng),
 			WithDefaultLanguage(language.English),
-			WithLoaders(ls...),
-		)
+		}
+
+		if len(opts) > 0 {
+			ops = append(ops, opts...)
+		}
+
+		i := New(ops...)
 
 		got, err := i.Translate(tt.args.lng, tt.args.msgID, tt.args.tplData)
 		if got != tt.want {
@@ -321,4 +375,117 @@ func runTranslationTest(t *testing.T, tt testTranslation, name string, ls ...Loa
 			}
 		}
 	})
+}
+
+func TestTranslateMessageLoadErrorLog(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		lng language.Tag
+		ls  []language.Tag
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    []string
+		notWant []string
+	}{
+		{
+			name: "all-exists",
+			args: args{
+				lng: language.English,
+				ls:  []language.Tag{language.German},
+			},
+			want: nil,
+		},
+		{
+			name: "no-exists",
+			args: args{
+				lng: language.English,
+				ls:  []language.Tag{language.French},
+			},
+			want: []string{
+				"level=WARN",
+				"lng=fr",
+				"load i18n message file",
+				ErrBundleLoadMessageFileFailed.Error(),
+			},
+			notWant: []string{
+				"level=ERROR",
+				"lng=en",
+				"load i18n default message file",
+			},
+		},
+		{
+			name: "no-exists-default",
+			args: args{
+				lng: language.French,
+				ls:  []language.Tag{language.English},
+			},
+			want: []string{
+				"level=ERROR",
+				"lng=fr",
+				"load i18n default message file",
+				ErrBundleLoadMessageFileFailed.Error(),
+			},
+			notWant: []string{
+				"level=WARN",
+				"lng=en",
+				"load i18n message file",
+			},
+		},
+		{
+			name: "no-exists-all",
+			args: args{
+				lng: language.French,
+				ls:  []language.Tag{language.English, language.Chinese},
+			},
+			want: []string{
+				"level=WARN",
+				"lng=zh",
+				"load i18n message file",
+				"level=ERROR",
+				"lng=fr",
+				"load i18n default message file",
+				ErrBundleLoadMessageFileFailed.Error(),
+			},
+			notWant: []string{
+				"lng=en",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		name := "MessageLoadErrorLog-" + tt.name
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			buf := new(strings.Builder)
+			l := slog.New(slog.NewTextHandler(buf, &slog.HandlerOptions{}))
+
+			_ = New(
+				WithLanguages(tt.args.ls...),
+				WithDefaultLanguage(tt.args.lng),
+				WithLoaders(EmbedLoader(testdata.Localize, "./localize/")),
+				WithLogger(l),
+			)
+
+			if len(tt.want) == 0 {
+				assert.Equal(t, "", buf.String())
+			} else {
+				for _, w := range tt.want {
+					assert.Contains(t, buf.String(), w)
+				}
+			}
+
+			if len(tt.notWant) > 0 {
+				for _, w := range tt.notWant {
+					assert.NotContains(t, buf.String(), w)
+				}
+			}
+
+			buf.Reset()
+		})
+	}
 }
